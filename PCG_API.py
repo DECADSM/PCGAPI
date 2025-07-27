65192c2b39f25fc74376b345e09b209ad1a6d51f
Ethan Villasin <duke129409@gmail.com>
2025-07-26 20:48:53 -0400

    Installing needed libraries, suggested by ChatGPT

---
+++ PCG_API.py
@@ -0,0 +1,6 @@
+import flask from Flask
+from flask_restful import Resource, Api
+
+#Launch venv for libraries
+
+#List of libraries downloaded: Flask, Flask-restful, noise, NumPy, anytree. SQLAlchemy, psycopg2, Flask-Caching
